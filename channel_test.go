09efdf7c4d457ef83864d29b8cf5f3d8fb9bc73a
wheelcomplex yin <wheelcomplex@gmail.com>
2016-05-07 11:56:40 +0800

    limit go routine to GOMAXPROCS for MPMC

--- channel_test.go
+++ channel_test.go
@@ -1,13 +1,14 @@
 package test
 
 import (
+	"runtime"
 	"sync"
 	"testing"
 
 	"github.com/Workiva/go-datastructures/queue"
 )
 
-func BenchmarkChannel(b *testing.B) {
+func BenchmarkChannelSPSC(b *testing.B) {
 	ch := make(chan interface{}, 1)
 
 	b.ResetTimer()
@@ -22,7 +23,7 @@ func BenchmarkChannel(b *testing.B) {
 	}
 }
 
-func BenchmarkRingBuffer(b *testing.B) {
+func BenchmarkChanRingBufferSPSC(b *testing.B) {
 	q := queue.NewRingBuffer(1)
 
 	b.ResetTimer()
@@ -37,102 +38,58 @@ func BenchmarkRingBuffer(b *testing.B) {
 	}
 }
 
-func BenchmarkChannelReadContention(b *testing.B) {
-	ch := make(chan interface{}, 100)
+func BenchmarkChannelMPMC(b *testing.B) {
+	ch := make(chan interface{}, 1)
 	var wg sync.WaitGroup
-	wg.Add(1000)
+	pn := runtime.GOMAXPROCS(-1)
+	wg.Add(pn)
+
 	b.ResetTimer()
-
-	go func() {
-		for i := 0; i < b.N; i++ {
-			ch <- `a`
-		}
-	}()
-
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < pn; i++ {
 		go func() {
-			for i := 0; i < b.N/1000; i++ {
+			for i := 0; i < b.N; i++ {
 				<-ch
 			}
-			wg.Done()
 		}()
 	}
 
-	wg.Wait()
-}
-
-func BenchmarkRingBufferReadContention(b *testing.B) {
-	q := queue.NewRingBuffer(100)
-	var wg sync.WaitGroup
-	wg.Add(1000)
-	b.ResetTimer()
-
-	go func() {
-		for i := 0; i < b.N; i++ {
-			q.Put(`a`)
-		}
-	}()
-
-	for i := 0; i < 1000; i++ {
-		go func() {
-			for i := 0; i < b.N/1000; i++ {
-				q.Get()
-			}
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-}
-
-func BenchmarkChannelContention(b *testing.B) {
-	ch := make(chan interface{}, 100)
-	var wg sync.WaitGroup
-	wg.Add(1000)
-	b.ResetTimer()
-
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < pn; i++ {
 		go func() {
 			for i := 0; i < b.N; i++ {
 				ch <- `a`
 			}
-		}()
-	}
-
-	for i := 0; i < 1000; i++ {
-		go func() {
-			for i := 0; i < b.N; i++ {
-				<-ch
-			}
 			wg.Done()
 		}()
 	}
-
 	wg.Wait()
 }
 
-func BenchmarkRingBufferContention(b *testing.B) {
-	q := queue.NewRingBuffer(100)
-	var wg sync.WaitGroup
-	wg.Add(1000)
-	b.ResetTimer()
+func BenchmarkChanRingBufferMPMC(b *testing.B) {
+	q := queue.NewRingBuffer(1)
 
-	for i := 0; i < 1000; i++ {
+	var wg sync.WaitGroup
+	pn := runtime.GOMAXPROCS(-1)
+	wg.Add(pn)
+
+	b.ResetTimer()
+	for i := 0; i < pn; i++ {
+		go func() {
+			go func() {
+				for i := 0; i < b.N; i++ {
+					q.Get()
+				}
+			}()
+
+		}()
+	}
+
+	for i := 0; i < pn; i++ {
 		go func() {
 			for i := 0; i < b.N; i++ {
 				q.Put(`a`)
 			}
-		}()
-	}
-
-	for i := 0; i < 1000; i++ {
-		go func() {
-			for i := 0; i < b.N; i++ {
-				q.Get()
-			}
 			wg.Done()
 		}()
 	}
-
 	wg.Wait()
 }
